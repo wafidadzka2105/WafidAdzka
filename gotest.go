f9d809b2d9e90a5a1e6f2ae55a88b7a69536ea5f
wafidadzka2105 <Adzkamukhayyar@gmail.com>
2024-03-04 16:25:33 +0700

    aritmatika

--- gotest.go
+++ gotest.go
@@ -1,9 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func tambah(a, b int) int {
+	return a + b
+}
 
 func main() {
-	fmt.Println("hello world")
+	angka1 := 5
+	angka2 := 3
+	hasil := tambah(angka1, angka2)
+
+	fmt.Printf("%d + %d = %d\n", angka1, angka2, hasil)
 }
